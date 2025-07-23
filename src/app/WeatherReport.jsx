04d472a9b0c2bb3764b0b66eb15d2f1408568dd6
Vijay Kanna <vjkanna24@gmail.com>
2025-07-23 15:10:47 +0530

    Page Dubug 2

--- src/app/WeatherReport.jsx
+++ src/app/WeatherReport.jsx
@@ -1,9 +1,15 @@
 'use client'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import weatherStyles from '@/styles/Weather.module.css';
 import { camelCase } from '@/utils/Functions';
 
 const WeatherReport = ({ userId, location, setLocation, getWeather, userBasedWeather, weatherData }) => {
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        if(weatherData) setData(weatherData);
+    }, [weatherData]);
 
     const unixToIST = (unixTimestamp) => {
         return new Intl.DateTimeFormat('en-IN', {
@@ -26,8 +32,6 @@ const WeatherReport = ({ userId, location, setLocation, getWeather, userBasedWea
     const celsiusToFahrenheit = (c) => {
         return ((c * 9 / 5) + 32).toFixed(2);
     }
-
-    console.log('Weather Data :', weatherData);
 
     return (
         <div className="glass-effect flex flex-wrap items-stretch justify-between w-full p-0 sm:p-4">
@@ -59,28 +63,28 @@ const WeatherReport = ({ userId, location, setLocation, getWeather, userBasedWea
                 }
             </div>
 
-            {weatherData &&
-                Object.keys(weatherData).length > 0 &&
+            {data &&
+                // Object.keys(weatherData).length > 0 &&
                 <div className="p-3 w-full h-full lg:w-2/3">
 
                     <div className={`currentWeather ${weatherStyles.gridBox} p-4`}>
 
                         <div className="currentWeather_location">
-                            <h1 className="text-xl font-bold text-gray-800">📍 {weatherData.location}</h1>
+                            <h1 className="text-xl font-bold text-gray-800">📍 {data.location}</h1>
                         </div>
 
                         <div className="currentWeather_condition flex flex-col md:flex-row items-center justify-between mb-6">
                             <div className="currentWeather_condition-weather flex items-center">
-                                <img src={`https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`} className='mr-2 w-32' />
+                                <img src={`https://openweathermap.org/img/wn/${data.icon}@2x.png`} className='mr-2 w-32' />
                                 <div className='flex flex-col'>
-                                    <p className="font-semibold text-gray-800 text-3xl">{camelCase(weatherData.weather)}</p>
-                                    <p className="text-md text-gray-600">{camelCase(weatherData.weatherDesc)}</p>
+                                    <p className="font-semibold text-gray-800 text-3xl">{camelCase(data.weather)}</p>
+                                    <p className="text-md text-gray-600">{camelCase(data.weatherDesc)}</p>
                                 </div>
                             </div>
                             <div className="currentWeather_condition-temprature text-center">
-                                <p className="text-5xl font-bold text-gray-800">{weatherData.temperature}°C</p>
-                                <p className="text-sm text-gray-500">Feels like {weatherData.feelsLike}°C</p>
-                                <p className="text-sm text-gray-500">↕ Min/Max: {weatherData.minTemp}°C / {weatherData.maxTemp}°C</p>
+                                <p className="text-5xl font-bold text-gray-800">{data.temperature}°C</p>
+                                <p className="text-sm text-gray-500">Feels like {data.feelsLike}°C</p>
+                                <p className="text-sm text-gray-500">↕ Min/Max: {data.minTemp}°C / {data.maxTemp}°C</p>
                             </div>
                         </div>
 
@@ -90,7 +94,7 @@ const WeatherReport = ({ userId, location, setLocation, getWeather, userBasedWea
                                     🌅 Sunrise
                                 </p>
                                 <p className="text-lg font-medium text-yellow-800">
-                                    {weatherData.sunrise !== undefined ? unixToIST(weatherData.sunrise) : '--:--'}
+                                    {data.sunrise !== undefined ? unixToIST(data.sunrise) : '--:--'}
                                 </p>
                             </div>
                             <div className="w-[48%] bg-orange-100 rounded-lg p-3 hover:bg-orange-200 transition col-span-1">
@@ -98,13 +102,13 @@ const WeatherReport = ({ userId, location, setLocation, getWeather, userBasedWea
                                     🌇 Sunset
                                 </p>
                                 <p className="text-lg font-medium text-orange-800">
-                                    {weatherData.sunset !== undefined ? unixToIST(weatherData.sunset) : '--:--'}
+                                    {data.sunset !== undefined ? unixToIST(data.sunset) : '--:--'}
                                 </p>
                             </div>
                         </div>
 
                         <div className="text-center text-sm text-gray-600 mt-4">
-                            ⏰ Last updated: {weatherData.lastUpdated !== undefined ? unixToIST(weatherData.lastUpdated) : '--:--'} IST
+                            ⏰ Last updated: {data.lastUpdated !== undefined ? unixToIST(data.lastUpdated) : '--:--'} IST
                         </div>
 
                     </div>
@@ -112,31 +116,31 @@ const WeatherReport = ({ userId, location, setLocation, getWeather, userBasedWea
                 </div>
             }
 
-            {weatherData &&
-                Object.keys(weatherData).length > 0 &&
+            {data &&
+                // Object.keys(weatherData).length > 0 &&
                 <div className="p-3 w-full lg:w-1/3 font-[var(--font-roboto)]">
                     <div className={`${weatherStyles.gridBox} p-4`}>
                         <div className="flex flex-wrap items-center mb-6">
                             <div className="bg-gray-50 rounded-lg p-3 hover:bg-gray-100 transition mr-2 mb-2">
                                 <p className="text-sm text-gray-600">💧 Humidity</p>
-                                <p className="text-lg font-medium text-gray-800">{weatherData.humidity}%</p>
+                                <p className="text-lg font-medium text-gray-800">{data.humidity}%</p>
                             </div>
                             <div className="bg-gray-50 rounded-lg p-3 hover:bg-gray-100 transition mr-2 mb-2">
                                 <p className="text-sm text-gray-600">🔭 Visibility</p>
-                                <p className="text-lg font-medium text-gray-800">{weatherData.visibility} km</p>
+                                <p className="text-lg font-medium text-gray-800">{data.visibility} km</p>
                             </div>
                             <div className="bg-gray-50 rounded-lg p-3 hover:bg-gray-100 transition mr-2 mb-2">
                                 <p className="text-sm text-gray-600">📈 Sea Level Pressure</p>
-                                <p className="text-lg font-medium text-gray-800">{weatherData.seaLevelPressure} hPa</p>
+                                <p className="text-lg font-medium text-gray-800">{data.seaLevelPressure} hPa</p>
                             </div>
                             <div className="bg-gray-50 rounded-lg p-3 hover:bg-gray-100 transition mr-2 mb-2">
                                 <p className="text-sm text-gray-600">📉 Ground Level Pressure</p>
-                                <p className="text-lg font-medium text-gray-800">{weatherData.groundLevelPressure} hPa</p>
+                                <p className="text-lg font-medium text-gray-800">{data.groundLevelPressure} hPa</p>
                             </div>
                             <div className="bg-gray-50 rounded-lg p-3 hover:bg-gray-100 transition mr-2 mb-2">
                                 <p className="text-sm text-gray-600">💨 Wind</p>
                                 <p className="text-lg font-medium text-gray-800">
-                                    {`${weatherData.windSpeed} (Gusts: ${weatherData.windGusts})`} from {windDegreeToDirection(weatherData.windDirection)}
+                                    {`${data.windSpeed} (Gusts: ${data.windGusts})`} from {windDegreeToDirection(data.windDirection)}
                                 </p>
                             </div>
                         </div>
